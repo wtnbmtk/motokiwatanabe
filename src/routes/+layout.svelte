4f44fb28a90b0c4e94d8f495c17920a5510e3aea
wtnbmtk <wtnbmtk7@gmail.com>
2024-02-23 02:45:25 +0900

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,12 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Menu from '$lib/components/Menu.svelte';
+	import icon from '$lib/assets/name-square-logo.svg';
 </script>
+
+<svelte:head>
+	<link rel="icon" href={icon} />
+</svelte:head>
 
 <Header />
 <main>
